300cda00edcd533fbd8b34bf451ea29da7745c1d
tihmstar <tihmstar@gmail.com>
2014-06-26 11:51:23 +0200

    found a cleaner way

--- deadP1nguin/ViewController.m
+++ deadP1nguin/ViewController.m
@@ -7,55 +7,16 @@
 //
 
 #import "ViewController.h"
-
-@interface NSTask : NSObject
-- (void)setLaunchPath:(NSString *)path;
-- (void)setStandardInput:(id)input;
-- (void)setStandardOutput:(id)output;
-- (void)setArguments:(NSArray *)arguments;
-- (void)setStandardError:(id)error;
-- (void)launch;
-@end
-@interface NSTask (NSTaskConveniences)
-- (void)waitUntilExit;
-@end
-
-
 @interface ViewController ()
             
 
 @end
 
 @implementation ViewController
-
--(NSString *)doSystemCallOnPath:(NSString*)launchPath withArguments:(NSArray *)arguments{
-    NSTask *task = [[NSTask alloc] init];
-    NSData *data;
-    
-    NSPipe *pipe = [NSPipe pipe];
-    NSFileHandle *file = [pipe fileHandleForReading];
-    
-    
-    [task setLaunchPath: launchPath];
-    if (arguments) {
-        [task setArguments: arguments];
-    }
-    [task setStandardError: pipe];
-    [task setStandardOutput: pipe];
-    
-    [task launch];
-    data = [file readDataToEndOfFile];
-    
-    [task waitUntilExit];
-    
-    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/panguaxe"]) {
-        [self doSystemCallOnPath:@"/panguaxe" withArguments:nil];
-    }
+    
+    system("/panguaxe");
     
     // Do any additional setup after loading the view, typically from a nib.
 }
